8b689df187728f54572e6312653b3ddd849822c0
tralaladudut <tralaladudut@gmail.com>
2019-05-31 15:10:20 +0800

    adding required feature to signup.php

--- pages/signup.php
+++ pages/signup.php
@@ -3,30 +3,75 @@
     $_SESSION['message'] = '';
     $mysqli = mysqli_connect('localhost', 'admin', 'admin', 'user');
 
+    $visible = 'none';
+
     //check connection to mysql
     if ($mysqli->connect_error) {
         die("Connection failed: " . $mysqli->connect_error);
     }
 
-    //process post
+    //if theres POST request process it
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // if password and confirm_password are equal process data...
         if ($_POST['password'] == $_POST['confirm_password']){
+            
+            // assign post data
             $username = $mysqli->real_escape_string($_POST['username']);
-            $password = md5($_POST['password']);
             $email = $mysqli->real_escape_string($_POST['email']);
+            $password = md5($_POST['password']); //md5 hash protection
 
-            $sql = "INSERT INTO accounts (username, password, email) VALUES ('$username','$password', '$email')";
-            if ($mysqli->query($sql)) {
-                $_SESSION['message'] = "Congratulations you have sign up successfully! Welcome $username!";
-                header("location: /pages/congrats.php");
-            } else {
-                $_SESSION['message'] = "User could not be added to the database!";
+            // check if username & password already exist...
+            $check_user_email = "SELECT * FROM accounts WHERE (username='$username' or email='$email')";
+            $respond = mysqli_query($mysqli, $check_user_email);
+
+            // if exists print alert message...
+            if (mysqli_num_rows($respond) > 0) {
+                $row = mysqli_fetch_assoc($respond);
+
+                if($username == $row['username']){
+
+                    $_SESSION['message'] = "Username already exist, Please choose other username";
+                    $visible = 'block';
+                    close_all($respond, $mysqli);
+
+                } elseif ($email == $row['email']) {
+
+                    $_SESSION['message'] = "Email already exist, Please use other email";
+                    $visible = 'block';
+                    close_all($respond, $mysqli);
+
+                }
+
+            // else query data...
+            } else {    
+                
+                $sql = "INSERT INTO accounts (username, password, email) VALUES ('$username','$password', '$email')";
+                
+                if ($mysqli->query($sql)) {
+                    // dsiplay congrats message
+                    $_SESSION['message'] = "Congratulations you have successfully sign up! Welcome $username!";
+                    $visible = 'block';
+                    header("location: /pages/congrats.php");
+                    mysqli_close($mysqli);
+
+                } else {
+
+                    $_SESSION['message'] = "User could not be added to the database!";
+                    $visible = 'block';
+                }
             }
 
         } else {
+
             $_SESSION['message'] = "Password confirmation failed!";
+            $visible = 'block';
         }
+    }
+
+    function close_all($respond, $mysqli) {
+        mysqli_free_result($respond);
+        mysqli_close($mysqli);
+        return null;
     }
 
 ?>
@@ -37,7 +82,11 @@
 <div class="signup">
     <form method="post">
         <h2>Sign Up</h2>
-        <div><?= $_SESSION['message'] ?></div>
+        <!-- alert box -->
+        <div class="alert" style="display: <?= $visible ?>">     
+            <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+                <strong><?= $_SESSION['message'] ?></strong>
+        </div>
         <input type="text" name="username" placeholder="Username" required>
         <br><br>
         <input type="password" name="password" placeholder="Password" required>
