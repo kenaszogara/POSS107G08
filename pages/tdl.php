ed2253ce881d37e5e34a0c869df24117836bb6a1
tralaladudut <tralaladudut@gmail.com>
2019-06-10 12:10:23 +0800

    update on tdl.php

--- pages/tdl.php
+++ pages/tdl.php
@@ -15,25 +15,63 @@ $today = date("Y-m-d");
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
         integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Sahitya' rel='stylesheet'>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
     <!-- pleas put all the .css file link below this tag-->
     <link rel="stylesheet" href="/css/tdl.css">
 
 </head>
+
+<style>
+    body {
+        line-height: 1.5;
+        font-family: "Montserrat";
+        font-size: 16px;
+    }
+    .custom-navbar {
+        background-color: #1B4A47;
+        font-family: "Montserrat", sans-serif;
+        color: #fff;
+        text-align: left;
+        box-shadow:0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12);
+        width: 100%;
+        overflow: hidden;
+        font-size: 18px;
+    }
+
+    .custom-header{
+        padding: 0.01em 16px;
+        color: #fff;
+        background-color: #1B4A47;
+        text-align: center;
+    }
+</style>
+
 <body>
-<header>
-<div class="container">
-        <img src="https://drive.google.com/thumbnail?id=14A-oheFQ2r8PRUnuVRQH5-K2r2ypNPt4" alt="logo" class="logo" width="65px">
-        <nav>
-            <ul>
-                <li><a href="about.html">About</a></li>
-                <li><a href="tdl.html">Home</a></li>
-				<li><a href="https://github.com/TKUIITFCChang/POSS107G08">Contact Us</a></li>
-                <li><a href="logout.html">Log out</a></li>
-            </ul>
-        </nav>
+
+    <!-- Navbar -->
+    <div class="w3-top">
+        <div class="custom-navbar">
+            <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red"
+                href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i
+                    class="fa fa-bars"></i></a>
+            <a href="/index.html" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
+            <a href="https://github.com/TKUIITFCChang/POSS107G08" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Contact Us</a>
+            <a href="logout.html" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Logout</a>
+        </div>
+
+        <!-- Navbar on small screens -->
+        <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+            <a href="logout.html" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Logout</a>
+            <a href="https://github.com/TKUIITFCChang/POSS107G08" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Contact Us</a>
+        </div>
     </div>
-</header>
+
+
+    <div class="custom-header" style="padding:128px 16px">
+        <img src="https://drive.google.com/thumbnail?id=14A-oheFQ2r8PRUnuVRQH5-K2r2ypNPt4" alt="logo" class="logo" width="65px">
+    </div>
+
 	<div id="date"><br>
 		<input type='date' id='hasta' value='<?php echo $today; ?>' class='textbox'>
 	</div>
