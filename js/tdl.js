7c337af6616c56bfb60a83dd77922a6b4744b26b
tralaladudut <tralaladudut@gmail.com>
2019-06-11 12:08:22 +0800

    fixing todos page

--- js/tdl.js
+++ js/tdl.js
@@ -1,13 +1,18 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
+
+// if press enter add todos
 $('input').keypress(function (event) {
     "use strict";
 	if (event.which === 13) {
 		var todoText = $(this).val();
+		var dateText = $('input[type=date]').val();
 		$(this).val("");
-		$('ul').append('<li>' + todoText + '<span><i class="fa fa-trash"</i></span>');
+		$('ul').append('<li>' + todoText + '   ' + dateText + '<span><i class="fa fa-trash"</i></span></li>');
 	}
 });
+
+// click on <span> fadeout and remove todo
 $('ul').on('click', "span", function (event) {
     "use strict";
 	$(this).parent().fadeOut(500, function () {
@@ -15,6 +20,8 @@ $('ul').on('click', "span", function (event) {
 	});
 	event.stopPropagation();
 });
+
+// click on <li> crossout todo
 $('ul').on('click', 'li', function () {
     "use strict";
 	$(this).toggleClass('done');
--- pages/tdl.php
+++ pages/tdl.php
@@ -12,12 +12,15 @@ $today = date("Y-m-d");
 <head>
     <title>To Do List</title>
     <!-- fonts cdns put below this tag -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
     <link href='https://fonts.googleapis.com/css?family=Sahitya' rel='stylesheet'>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
     <!-- pleas put all the .css file link below this tag-->
-    <link rel="stylesheet" href="/css/tdl.css">
+    <link rel="stylesheet" href="../css/tdl.css">
+    <link rel="stylesheet" href="../css/skeleton.css">
+    <link rel="stylesheet" href="../css/normalize.css">
 
 </head>
 
@@ -44,6 +47,30 @@ $today = date("Y-m-d");
         background-color: #1B4A47;
         text-align: center;
     }
+
+    .content-view{
+        padding: 18px;
+    }
+
+    .custom-content{
+        width: 400px;
+        box-shadow: 0 0 3px rgba(0,0,0, .5);
+        font-size: 19px;
+        margin: 40px auto;
+        padding: 12px;
+        background: #BE624C;
+    }
+    .custom-padding{
+        padding: 8px;
+    }
+    h2{
+        color: white;
+    }
+    .row{
+        margin: auto;
+    }
+
+    
 </style>
 
 <body>
@@ -71,16 +98,36 @@ $today = date("Y-m-d");
         <img src="https://drive.google.com/thumbnail?id=14A-oheFQ2r8PRUnuVRQH5-K2r2ypNPt4" alt="logo" class="logo" width="65px">
     </div>
 
-	<div id="date"><br>
-		<input type='date' value='<?php echo $today; ?>' class='textbox'>
-	</div>
-	<div id="container">
-		<input type="text" placeholder="Add a to-do" autocomplete="off" autofocus>
-		<ul>
-			<li>Sign up<span><i class="fa fa-trash"></i></span></li>
-			<li>Log in<span><i class="fa fa-trash"></i></span></li>
-			<li>Add to-dos<span><i class="fa fa-trash"></i></span></li>
-		</ul>
-	</div>
-	<script type="text/javascript" src="js/tdl.js"></script>
+    <div class="row">
+        <div class="six columns">
+            <div class="custom-content">
+                <div class="custom-padding">
+                    <h2>Add Todo</h2>
+                </div>
+                <div class="container">
+                    <div class="custom-padding">
+                        <input type='date' value='<?php echo $today; ?>' class='textbox'>
+                    </div>
+                    <div class="custom-padding">
+                        <input type="text" placeholder="Add a to-do" autocomplete="off" required>
+                    </div>
+                </div>  
+            </div>
+        </div>
+
+        <div class="two columns">
+            <div id="container">  
+                <ul>
+                    <li>Sign up<span><i class="fa fa-trash"></i></span></li>
+                    <li>Log in<span><i class="fa fa-trash"></i></span></li>
+                    <li>Add to-dos<span><i class="fa fa-trash"></i></span></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+    
+	
+    <script type="text/javascript" src="../js/tdl.js"></script>
+    
 </body>
