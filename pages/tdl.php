c471a2c170c43a859f4eb59b28d3cde03532f7e0
tralaladudut <tralaladudut@gmail.com>
2019-06-12 11:54:01 +0800

    fixing reset_pass & tdl.php

--- pages/tdl.php
+++ pages/tdl.php
@@ -1,9 +1,34 @@
 <?php 
 // if no user login, 404
-if ( !isset($_SESSION['username']) ) {
-	header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+session_start();
+
+require_once "../scripts/db.php";
+
+if(!isset($_SESSION['username'])){
+    header('location: /pages/login.html');
 }
 
+if (isset($_GET['del_task'])) {
+	$id = $_GET['del_task'];
+
+	mysqli_query($mysqli, "DELETE FROM tasks WHERE id=".$id);
+	header('location: tdl.php');
+}
+
+$username = $_SESSION['username'];
+
+$accounts = mysqli_query($mysqli, "SELECT user_id FROM accounts WHERE (username='$username')" );
+
+$a_row = mysqli_fetch_array($accounts);
+
+$userID = $a_row['user_id'];
+
+// insert a tasks if submit button is clicked
+if (isset($_POST['task'])) {
+    $task = $_POST['task'];
+    $stmt = "INSERT INTO tasks (description, user_id) VALUES ('$task', '$userID')";
+    mysqli_query($mysqli, $stmt);
+}
 
 ?>
 
@@ -45,6 +70,8 @@ if ( !isset($_SESSION['username']) ) {
         color: #fff;
         background-color: #1B4A47;
         text-align: center;
+        font-family: "Montserrat", sans-serif;
+        font-size: 34px;
     }
 
     .content-view{
@@ -65,6 +92,31 @@ if ( !isset($_SESSION['username']) ) {
     h2{
         color: white;
     }
+    table {
+        width: 50%;
+        margin: 30px auto;
+        border-collapse: collapse;
+    }
+
+    tr {
+        border-bottom: 1px solid #cbcbcb;
+    }
+
+    th {
+        font-size: 19px;
+        color: #6B8E23;
+    }
+
+    th, td{
+        border: none;
+        height: 30px;
+        padding: 2px;
+    }
+
+    tr:hover {
+        background: #E9E9E9;
+    }
+
     .row{
         margin: auto;
     }
@@ -82,19 +134,19 @@ if ( !isset($_SESSION['username']) ) {
                     class="fa fa-bars"></i></a>
             <a href="/index.html" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
             <a href="https://github.com/TKUIITFCChang/POSS107G08" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Contact Us</a>
-            <a href="logout.html" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Logout</a>
+            <a href="logout.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Logout</a>
         </div>
 
         <!-- Navbar on small screens -->
         <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-            <a href="logout.html" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Logout</a>
+            <a href="logout.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Logout</a>
             <a href="https://github.com/TKUIITFCChang/POSS107G08" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Contact Us</a>
         </div>
     </div>
 
 
     <div class="custom-header" style="padding:128px 16px">
-        <img src="https://drive.google.com/thumbnail?id=14A-oheFQ2r8PRUnuVRQH5-K2r2ypNPt4" alt="logo" class="logo" width="65px">
+       <h1>Welcome <?=$_SESSION['username']?>!</h1>
     </div>
 
     <div class="row">
@@ -105,19 +157,42 @@ if ( !isset($_SESSION['username']) ) {
                 </div>
                 <div class="container">
                     <div class="custom-padding">
-                        <input type="text" placeholder="Add a to-do" autocomplete="off" required>
+                        <form action="tdl.php" method="POST" style="padding: 8px">
+                        <input name="task" type="text" placeholder="Add a to-do" autocomplete="off" required>
+                        <input class="button-primary" type="submit" value="ADD">
+                        </form>
                     </div>
                 </div>  
             </div>
         </div>
 
         <div class="two columns">
-            <div id="todo-lists">  
-                <ul>
-                    <li>Sign up<span><i class="fa fa-trash"></i></span></li>
-                    <li>Log in<span><i class="fa fa-trash"></i></span></li>
-                    <li>Add to-dos<span><i class="fa fa-trash"></i></span></li>
-                </ul>
+            <div id="todo-lists" style="display: table">  
+                <table>
+                    <thead>
+                        <tr>
+                            <th>N</th>
+                            <th>Tasks</th>
+                            <th style="width: 60px;">Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php 
+                        // select all tasks if page is visited or refreshed
+                        $tasks = mysqli_query($mysqli, "SELECT * FROM tasks where user_id='$userID' ");
+
+                        $i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
+                            <tr>
+                                <td> <?php echo $i; ?> </td>
+                                <td> <?php echo $row['description']; ?> </td>
+                                <td> 
+                                    <a href="tdl.php?del_task=<?php echo $row['id'] ?>">x</a> 
+                                </td>
+                            </tr>
+                        <?php $i++; } ?>	
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
